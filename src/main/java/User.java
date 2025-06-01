3fb6642114e6187184f534b677fb5f05b7bb4ed8
Max <max@staneker.com>
2025-06-01 02:07:24 +0200

    Create User.java

---
+++ src/main/java/User.java
@@ -0,0 +1,20 @@
+public class User {
+    private String name;
+    private long lastLoginTime;
+
+    public User(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public long getLastLoginTime() {
+        return lastLoginTime;
+    }
+    public void setLastLoginTime(long lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+}
