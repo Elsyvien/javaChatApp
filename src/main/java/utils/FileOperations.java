3f10e4a20febdf579e78d48441180e1be9cfc5e8
Max <max@staneker.com>
2025-06-18 00:33:41 +0200

    Create FileOperations.java

---
+++ src/main/java/utils/FileOperations.java
@@ -0,0 +1,19 @@
+package utils;
+
+/*
+ * Small utility class for file operations.
+ * This class is intended to handle file-related operations such as reading and writing files.
+ * It can be extended in the future to include more complex file handling functionalities.
+ * @author Max Staneker, Mia Schienagel
+ * @version 0.1
+ */
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+
+public class FileOperations {
+
+}
